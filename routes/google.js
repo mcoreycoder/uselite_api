8bdb6780134e88a1f93306419316aad59d6905fb
Michael Corey <fishincorey@gmail.com>
2021-07-09 11:51:50 -0600

    updated google file structure

--- routes/google.js
+++ routes/google.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 require('dotenv').config()
 
-const { google } = require('googleapis')
+const {authorize, listBrands, listGSA, listArcteryx, listArcteryxVariants} = require('../google/google_functions')
+
+
 
 const access_token = `${process.env.ACCESS_TOKEN}`
 const googleServer = {
@@ -20,56 +22,15 @@ const googleServer = {
 
 let brandsList = ['empty']
 console.log(`0 brandsList: ${brandsList}`)
+
 let gsaList = ['empty']
 console.log(`0 gsaList: ${gsaList}`)
 
-async function authorize (credentials) {
-  const { client_secret, client_id, redirect_uris } = credentials.server
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  )
+let arcteryxList = ['empty']
+console.log(`0 arcteryxList: ${arcteryxList}`)
 
-  await oAuth2Client.setCredentials(JSON.parse(access_token))
-  return oAuth2Client
-}
 
-async function listBrands (auth) {
-  let brandArray = ['test']
-  const sheets = google.sheets({ version: 'v4', auth })
-
-  try {
-    brandArray = (
-      await sheets.spreadsheets.values.get({
-        spreadsheetId: '1QKb7y3UNOqdenom37XscsSNk7REUNsHKy-qfXWosNTo',
-        range: 'MasterVendorBrands!A3:A8'
-      })
-    ).data.values
-  } catch (err) {
-    console.error(`listBrands Oh Sheet! and err:\n`, err)
-  }
-  return brandArray
-}
-
-async function listGSA (auth) {
-  let gsaArray = []
-  const sheets = google.sheets({ version: 'v4', auth })
-
-  try {
-    gsaArray = (
-      await sheets.spreadsheets.values.get({
-        spreadsheetId: '1wGPTT10lpextZn0A4LGNuqhrRqLDJC7ysBGEpAY9Xks',
-        range: '2021ProductList!Q3:V8'
-      })
-    ).data.values
-  } catch (err) {
-    console.error(`listBrands Oh Sheet! and err:\n`, err)
-  }
-  return gsaArray
-}
-
-authorize(googleServer).then(userAuthed => {
+authorize(googleServer, access_token).then(userAuthed => {
   router.get('/brands', async function (req, res, next) {
     let readObj = {
       // usersCollection: req.app.locals.usersCollection,
@@ -113,6 +74,52 @@ authorize(googleServer).then(userAuthed => {
         res.status(500).json(error)
       })
     return res.send(gsaList)
+  })
+
+  router.get('/arcteryx', async function (req, res, next) {
+    let readObj = {
+      // usersCollection: req.app.locals.usersCollection,
+      resource: req.baseUrl.slice(1) // need to add this to other routes and on users.js
+    }
+    console.log(`check arcteryxList: ${arcteryxList[0]}`)
+
+    if (arcteryxList[0] === 'empty') {
+    }
+    // let userAuthed = await authorize(googleServer)
+    arcteryxList = await listArcteryx(userAuthed)
+      .then(response => {
+        console.log(`google get response: ${response[0]}`)
+        arcteryxList = response
+        return arcteryxList
+      })
+      .catch(error => {
+        console.log('Error:', error)
+        res.status(500).json(error)
+      })
+    return res.send(arcteryxList)
+  })
+  
+  router.get('/listArcteryxVariants', async function (req, res, next) {
+    let readObj = {
+      // usersCollection: req.app.locals.usersCollection,
+      resource: req.baseUrl.slice(1) // need to add this to other routes and on users.js
+    }
+    console.log(`check arcteryxList: ${arcteryxList[0]}`)
+
+    if (arcteryxList[0] === 'empty') {
+    }
+    // let userAuthed = await authorize(googleServer)
+    arcteryxList = await listArcteryxVariants(userAuthed)
+      .then(response => {
+        console.log(`google get response: ${response[0]}`)
+        arcteryxList = response
+        return arcteryxList
+      })
+      .catch(error => {
+        console.log('Error:', error)
+        res.status(500).json(error)
+      })
+    return res.send(arcteryxList)
   })
 })
 
